8087d2a7a92da7ce19d3c303dac9d897d125a0fa
Rebecca Crompton <r.crompton@live.co.za>
2024-05-15 20:49:26 +0200

    Add s3 bucket and configuration

---
+++ infra/s3.tf
@@ -0,0 +1,26 @@
+# Create S3 bucket to store survey data
+
+resource "aws_s3_bucket" "raw_survey_data_bucket" {
+  bucket = "raw-survey-landing-data"
+}
+
+# S3 bucket configuration: ownership controls, ACL, and event notifications
+
+resource "aws_s3_bucket_ownership_controls" "ownership_controls" {
+  bucket = aws_s3_bucket.raw_survey_data_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "raw_survey_data_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.ownership_controls]
+
+  bucket = aws_s3_bucket.raw_survey_data_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket      = aws_s3_bucket.raw_survey_data_bucket.id
+  eventbridge = true
+}

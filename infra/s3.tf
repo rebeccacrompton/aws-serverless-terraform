b6364b640aee0f1e44d008f9a6e8bf37c252c396
Rebecca Crompton <r.crompton@live.co.za>
2024-05-16 17:44:02 +0200

    Update bucket names

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,10 +1,10 @@
 module "s3_bucket" {
   source      = "./modules/s3_bucket"
-  bucket_name = "raw-survey-landing-data"
+  bucket_name = "demo-raw-survey-landing-data"
   eventbridge = true
 }
 
 module "survey_results_data_bucket" {
   source      = "./modules/s3_bucket"
-  bucket_name = "survey-results-data"
+  bucket_name = "demo-survey-results-data"
 }
